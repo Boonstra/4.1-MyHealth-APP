7e3f515e919d6aa5edcec140742b76f80e101700
jorian99 <jorian_09@hotmail.com>
2013-09-17 12:37:33 +0200

    Inlog scherm af

--- gen/com/example/myhealth/R.java
+++ gen/com/example/myhealth/R.java
@@ -24,28 +24,52 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_forgot_password=0x7f080007;
+        public static final int action_settings=0x7f080008;
+        public static final int login=0x7f080005;
+        public static final int login_form=0x7f080002;
+        public static final int login_status=0x7f080000;
+        public static final int login_status_message=0x7f080001;
+        public static final int password=0x7f080004;
+        public static final int sign_in_button=0x7f080006;
+        public static final int username=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int login=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f050008;
         public static final int action_settings=0x7f050001;
+        public static final int action_sign_in=0x7f050006;
+        public static final int action_sign_in_short=0x7f050007;
         public static final int app_name=0x7f050000;
+        public static final int error_field_required=0x7f05000c;
+        public static final int error_incorrect_password=0x7f05000b;
+        public static final int error_invalid_password=0x7f05000a;
         public static final int hello_world=0x7f050002;
+        public static final int login_progress_signing_in=0x7f050009;
+        /**  Strings related to login 
+         */
+        public static final int prompt_password=0x7f050004;
+        public static final int prompt_username=0x7f050005;
+        public static final int title_activity_login=0x7f050003;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -64,5 +88,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int LoginFormContainer=0x7f060002;
     }
 }
